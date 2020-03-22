665e5102de34aad2c4ec8e6f9899152132b14be9
Laura Eckman <leckman@mit.edu>
2020-03-21 22:54:31 -0700

    organize neighborhoods and regions in LA county

--- src/CityData/Areas.js
+++ src/CityData/Areas.js
@@ -1,8 +1,20 @@
 const Areas = {
-  sf: "San Francisco",
-  eastbay: "East Bay",
-  southbay: "South Bay",
-  marin: "Marin"
+  antelope_valley: "Antelope Valley",
+  south_la: "South L.A.",
+  santa_monica_mountains: "Santa Monica Mountains",
+  northwest_county: "Northwest County",
+  san_gabriel_valley: "San Gabriel Valley",
+  south_bay: "South Bay",
+  verdugos: "Verdugos",
+  angeles_forest: "Angeles Forest",
+  san_fernando_valley: "San Fernando Valley",
+  central_la: "Central L.A.",
+  southeast: "Southeast",
+  northeast_la: "Northeast L.A.",
+  harbor: "Harbor",
+  westside: "Westside",
+  eastside: "Eastside",
+  pomona_valley: "Pomona Valley"
 };
 
 export default Areas;
